0fe51b3ac13a1ff312c9d639f66c828b2e69ea90
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-09-26 18:36:49 -0400

    Separate show pages for each Pokemon via image on index

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const Pokemon = require('./models/pokemon.js');
 const app = express();
 const port = 3000;
 
-
+// ROUTES //
 // INDEX
 app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { allPokemon: Pokemon });
 });
 // I want this page to only display the images of my Pokemon 
 
-
-
 // SHOW
-app.get('/:indexOfPokemon', (req, res) => {
+app.get('/pokemon/:indexOfPokemon', (req, res) => {
 res.render('show.ejs', { data: Pokemon[req.params.id] });
 });
+// I want this page to display name, image, unordered list of types, and stats
 
 app.listen(3000);
