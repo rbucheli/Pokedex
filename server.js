23aac52d9f38cc4d8c9ad7e705d1b59361768b87
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-09-26 22:56:37 -0400

    Added Pokemon info

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ res.render('index.ejs', { allPokemon: Pokemon });
 // I want this page to only display the images of my Pokemon 
 
 // SHOW
-app.get('/pokemon/:indexOfPokemon', (req, res) => {
-res.render('show.ejs', { data: Pokemon[req.params.id] });
+app.get('/pokemon/:index', (req, res) => {
+res.render('show.ejs', { allPokemon: Pokemon[req.params.index] });
 });
 // I want this page to display name, image, unordered list of types, and stats
+// ejs syntax problem on show,ejs
 
 app.listen(3000);
