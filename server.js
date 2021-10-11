bc541bbc9df266f2d59f8eda16d496b4b6e2580a
hum-motor-whir <rodrigo.bucheli@gmail.com>
2021-10-11 00:46:34 -0400

    Update and delete complete

--- server.js
+++ server.js
@@ -1,20 +1,59 @@
+// DEPENDENCIES //
 const express = require('express');
 const Pokemon = require('./models/pokemon.js');
 const app = express();
+const methodOverride = require('method-override');
+const pokemon = require('./models/pokemon.js');
 const port = 3000;
 
+// DATABASE
+
+// MODELS
+
+// MIDDLEWARE
+app.use(methodOverride('_method'));
+app.use((req, res, next) => {
+  console.log('I run for all routes');
+  next();
+});
+app.use(express.urlencoded({ extended:false }));
+
+
 // ROUTES //
-// INDEX
+// INDEX (get)
 app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { allPokemon: Pokemon });
 });
 // I want this page to only display the images of my Pokemon 
 
-// SHOW
+// NEW (get)
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+// DESTROY (delete)
+
+
+// UPDATE (put)
+
+// CREATE (post)
+app.post('/pokemon', (req, res) => {
+  pokemon.push(req.body);
+  res.redirect('/pokemon');
+});
+
+// EDIT (get) (put)
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+// SHOW (get)
 app.get('/pokemon/:index', (req, res) => {
 res.render('show.ejs', { allPokemon: Pokemon[req.params.index] });
 });
 // I want this page to display name, image, unordered list of types, and stats
-// ejs syntax problem on show,ejs
 
+
+
+// LISTENER //
 app.listen(3000);
